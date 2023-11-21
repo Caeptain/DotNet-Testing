30a15ba61da4b9e42f550f3953b5618c2e725a4a
Maximilian Wunderlich <Maximilian.Wunderlich@werma.com>
2023-11-21 10:19:14 +0100

    Added Tests for durable tasks

---
+++ projects/DurableEntitiesT/DurableEntitiesT/Counter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.DurableTask;
+using Microsoft.Extensions.Logging;
+
+[DurableTask(nameof(Counter))]
+public class Counter : TaskActivity<int, int>
+{
+    private readonly ILogger logger;
+    public int Count { get; set; }
+
+    public Counter(ILogger<Counter> logger)
+    {
+        this.logger = logger;
+    }
+
+    public override Task<int> RunAsync(TaskActivityContext context, int input)
+    {
+        logger.LogInformation($"Counter received {input}");
+        Count++;
+        return Task.FromResult(Count);
+    }
+}
---
+++ projects/DurableEntitiesT/DurableEntitiesT/MyOrchestration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.DurableTask;
+using Microsoft.Extensions.Logging;
+
+[DurableTask(nameof(MyOrchestration))]
+public class MyOrchestration : TaskOrchestrator<int, int>
+{
+    public override async Task<int> RunAsync(TaskOrchestrationContext context, int input)
+    {
+        var logger = context.CreateReplaySafeLogger<MyOrchestration>();
+        logger.LogInformation($"MyOrchestration received {input}");
+
+        return await context.CallCounterAsync(input);
+    }
+}
